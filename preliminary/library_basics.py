67229216129a19e112ea5b88f1ec64165bfed2ce
HeyWray <mxthomaswray@gmail.com>
2025-10-28 15:45:58 +0800

    feat:

--- preliminary/library_basics.py
+++ preliminary/library_basics.py
@@ -11,27 +11,26 @@ Some docstrings **require** you to add references to third-party documentation.
 Make sure you read the docstrings C.A.R.E.F.U.L.Y (yes, I took the L to check that you are awake!)
 """
 
-# imports - add all required imports here
+from PIL import Image
 from pathlib import Path
 import cv2
 import numpy as np
 
+VID_PATH = Path("../resources/oop.mp4")
+OUT_PATH = Path("../resources")
 
-VID_PATH = Path("resources/name-of-vid-given-to-you-by-instructor.mp4")
 
 class CodingVideo:
     capture: cv2.VideoCapture
 
-
     def __init__(self, video: Path | str):
-        self.capture = ... # You complete me!
+        self.capture = cv2.VideoCapture(video)  # You complete me!
         if not self.capture.isOpened():
             raise ValueError(f"Cannot open {video}")
 
-        self.fps = ...
-        self.frame_count = ...
-        self.duration = ...
-
+        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
+        self.frame_count = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.duration = self.frame_count / self.fps
 
     def __str__(self) -> str:
         """Displays key metadata from the video
@@ -45,10 +44,12 @@ class CodingVideo:
         ----------
         https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
         """
+        return (f"Video is {round(self.duration / 60, 2)} minutes long, "
+                f"with {self.frame_count} total frames, at {round(self.fps, 2)} FPS")
 
     def get_frame_number_at_time(self, seconds: int) -> int:
         """Given a time in seconds, returns the value of the nearest frame"""
-
+        return int(seconds * self.fps)
 
     def get_frame_rgb_array(self, frame_number: int) -> np.ndarray:
         """Returns a numpy N-dimensional array (ndarray)
@@ -56,12 +57,12 @@ class CodingVideo:
         The array represents the RGB values of each pixel in a given frame
 
         Note: cv2 defaults to BGR format, so this function converts the color space to RGB
-
-        Reference
-        ---------
-        # TODO: Find a tutorial on OpenCV that demonstrates color space conversion
-
         """
+        self.capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+        ok, frame = self.capture.read()
+        if not ok:
+            raise ValueError("Unable to read frame from file")
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     def get_image_as_bytes(self, seconds: int) -> bytes:
         self.capture.set(cv2.CAP_PROP_POS_FRAMES, self.get_frame_number_at_time(seconds))
@@ -73,22 +74,22 @@ class CodingVideo:
             raise ValueError("Failed to encode frame")
         return buf.tobytes()
 
-
-
-
     def save_as_image(self, seconds: int, output_path: Path | str = 'output.png') -> None:
-      """Saves the given frame as a png image
+        """Saves the given frame as a png image"""
+        if type(output_path) is str:
+            output_path = OUT_PATH / output_path
 
-      # TODO: Requires a third-party library to convert ndarray to png
-      # TODO: Identify the library and add a reference to its documentation
+        frame = self.get_frame_number_at_time(seconds)
+        frame = self.get_frame_rgb_array(frame)
+        image = Image.fromarray(frame)
+        image.save(output_path)
 
-
-      """
 def test():
     """Try out your class here"""
-    oop = CodingVideo("resources/oop.mp4")
+    oop = CodingVideo(VID_PATH)
     print(oop)
     oop.save_as_image(42)
+
 
 if __name__ == '__main__':
     test()
